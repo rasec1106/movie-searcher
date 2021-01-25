ed7d808d72eae819c2a1017e0b82837ef7e79002
Rasec <elch.herrera@gmail.com>
2021-01-25 17:29:45 -0500

    add toDo list

--- js/searcher.js
+++ js/searcher.js
@@ -1,28 +1,68 @@
-/**
- * I  needed to register in the website and request for an API token
- */
-var api = {
-	root: "https://api.themoviedb.org/3", //the web page
-	token: "2e24785394d993f26fba2a923499635d" // my API key
-}
-
-/**
- * Makes an AJAX request to themoviedb.org, asking for some movies
- * if successful, prints the results to the console
- */
-function testTheAPI() {
-	$.ajax({
-		url: api.root + "/discover/movie",
-		data: {
-			api_key: api.token,
-		},
-		success: function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
-		}
-	});
-}
-
-
-console.log("The script loaded!");
-testTheAPI();
+var model = {
+    watchlistItems: [],
+    browseItems: []
+  }
+  
+  
+  var api = {
+    root: "https://api.themoviedb.org/3",
+    token: "TODO" // TODO 0 put your api key here
+  }
+  
+  
+  /**
+   * Makes an AJAX request to themoviedb.org, asking for some movies
+   * if successful, updates the model.browseItems appropriately, and then invokes
+   * the callback function that was passed in
+   */
+  function discoverMovies(callback) {
+      $.ajax({
+          url: api.root + "/discover/movie",
+          data: {
+              api_key: api.token,
+          },
+          success: function(response) {
+              console.log("We got a response from The Movie DB!");
+              console.log(response);
+              
+              // TODO 2
+              // update the model, setting its .browseItems property equal to the movies we recieved in the response
+              
+              // invoke the callback function that was passed in. 
+              callback();
+          }
+      });
+    
+  }
+  
+  
+  /**
+   * re-renders the page with new content, based on the current state of the model
+   */
+  function render() {
+    // TODO 7
+    // clear everything from both lists
+    
+    // TODO 6
+    // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
+    
+    // for each movie on the current browse list, 
+    model.browseItems.forEach(function(movie) {
+          // TODO 3
+          // insert a list item into the <ul> in the browse section
+          
+          // TODO 4
+          // the list item should include a button that says "Add to Watchlist"
+          
+          // TODO 5
+          // when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+    });
+    
+  }
+  
+  
+  // When the HTML document is ready, we call the discoverMovies function,
+  // and pass the render function as its callback
+  $(document).ready(function() {
+    discoverMovies(render);
+  });
